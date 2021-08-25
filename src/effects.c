a52701c11418c61876dbf6a732dd44e611581ce5
Jan <jan@moddevices.com>
2021-08-25 12:06:14 +0200

    handle noisegate mono behaviour correctly again

--- src/effects.c
+++ src/effects.c
@@ -2599,9 +2599,6 @@ static int ProcessGlobalClient(jack_nframes_t nframes, void *arg)
     /* */ float *const audio_out1_buf = (float*)jack_port_get_buffer(g_audio_out1_port, nframes);
     /* */ float *const audio_out2_buf = (float*)jack_port_get_buffer(g_audio_out2_port, nframes);
 
-    for (uint32_t i=0; i<nframes; ++i)
-        gate_push_sample(&g_noisegate, audio_in1_buf[i], audio_in2_buf[i]);
-
     switch (g_noisegate_channel)
     {
     case 0: // no gate
@@ -2611,16 +2608,23 @@ static int ProcessGlobalClient(jack_nframes_t nframes, void *arg)
     case 1: // left channel only
         memcpy(audio_out2_buf, audio_in2_buf, sizeof(float)*nframes);
         for (uint32_t i=0; i<nframes; ++i)
+        {
+            gate_push_sample(&g_noisegate, audio_in1_buf[i], 0.f);
             audio_out1_buf[i] = gate_run(&g_noisegate, audio_in1_buf[i]);
+        }
         break;
     case 2: // right channel only
         memcpy(audio_out1_buf, audio_in1_buf, sizeof(float)*nframes);
         for (uint32_t i=0; i<nframes; ++i)
+        {
+            gate_push_sample(&g_noisegate, 0.f, audio_in2_buf[i]);
             audio_out2_buf[i] = gate_run(&g_noisegate, audio_in2_buf[i]);
+        }
         break;
     case 3: // left & right channels
         for (uint32_t i=0; i<nframes; ++i)
         {
+            gate_push_sample(&g_noisegate, audio_in1_buf[i], audio_in2_buf[i]);
             audio_out1_buf[i] = gate_run(&g_noisegate, audio_in1_buf[i]);
             audio_out2_buf[i] = gate_apply(&g_noisegate, audio_in2_buf[i]);
         }
